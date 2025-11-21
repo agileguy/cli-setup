b22aabc63f9870d4af9c713fe4a91b7494a8b4e8
agileguy <agile.guy@hotmail.com>
2025-11-21 12:16:06 -0700

    Revert "putting back tmux.conf"
    This reverts commit 11dfb0c36a049cc8367bf61d76de1d86f9f9cbef.

--- install.sh
+++ install.sh
@@ -21,16 +21,6 @@ curl -o ~/.bashrc https://raw.githubusercontent.com/agileguy/cli-setup/refs/head
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 git clone https://github.com/magicmonty/bash-git-prompt.git ~/.bash-git-prompt --depth=1
 
-platform="linux"  # or "macos"
-
-# Download
-wget https://github.com/sayanarijit/xplr/releases/latest/download/xplr-$platform.tar.gz
-
-# Extract
-tar xzvf xplr-$platform.tar.gz
-
-# Place in $PATH
-sudo mv xplr /usr/local/bin/
 source ~/.bashrc
 
 
