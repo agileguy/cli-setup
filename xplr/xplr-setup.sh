7fbbda56244ef635d79db9ac80dab8bd57f4a8c1
agileguy <agile.guy@hotmail.com>
2025-11-25 21:09:54 -0700

    set location of wget

--- xplr/xplr-setup.sh
+++ xplr/xplr-setup.sh
@@ -1,9 +1,9 @@
 platform="linux"  # or "macos"
 # Download
-wget https://github.com/sayanarijit/xplr/releases/latest/download/xplr-$platform.tar.gz
+wget -o ./xplr-$platform.tar.gz https://github.com/sayanarijit/xplr/releases/latest/download/xplr-$platform.tar.gz
 # Extract
-tar xzvf xplr-$platform.tar.gz
+tar xzvf ./xplr-$platform.tar.gz
 # Place in $PATH
 sudo mv xplr /usr/local/bin/
-rm xplr-$platform.tar.gz
+rm ./xplr-$platform.tar.gz
 curl -o ~/.config/xplr/init.lua https://raw.githubusercontent.com/agileguy/cli-setup/refs/heads/main/xplr/init.lua
