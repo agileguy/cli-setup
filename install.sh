da438082a1cdef07e2dced45768383cf65df9dcc
agileguy <agile.guy@hotmail.com>
2025-11-25 20:50:04 -0700

    xplr init lua

--- install.sh
+++ install.sh
@@ -25,16 +25,14 @@ git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 git clone https://github.com/magicmonty/bash-git-prompt.git ~/.bash-git-prompt --depth=1
 
 platform="linux"  # or "macos"
-
 # Download
 wget https://github.com/sayanarijit/xplr/releases/latest/download/xplr-$platform.tar.gz
-
 # Extract
 tar xzvf xplr-$platform.tar.gz
-
 # Place in $PATH
 sudo mv xplr /usr/local/bin/
 rm xplr-$platform.tar.gz
+curl -o ~/.config/xplr/init.lua https://raw.githubusercontent.com/agileguy/cli-setup/refs/heads/main/xplr/init.lua
 
 source ~/.bashrc
 
---
+++ xplr/init.lua
@@ -0,0 +1,16 @@
+version = "1.0.1"
+xplr.config.layouts.builtin.default = {
+	Horizontal = {
+		config = {
+			margin = 1,
+			horizontal_margin = 1,
+			vertical_margin = 1,
+			constraints = {
+				{ Percentage = 100 },
+			},
+		},
+		splits = {
+			"Table",
+		},
+	},
+}
