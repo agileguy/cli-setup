bdd09a326d593c133eb95ea93b056d1179c91e1e
agileguy <agile.guy@hotmail.com>
2025-11-25 21:21:56 -0700

    correct directory for removals

--- xplr-setup/xplr-setup.sh
+++ xplr-setup/xplr-setup.sh
@@ -4,6 +4,6 @@ wget https://github.com/sayanarijit/xplr/releases/latest/download/xplr-$platform
 # Extract
 tar xzvf xplr-$platform.tar.gz
 # Place in $PATH
-sudo mv ~/Downloads/xplr /usr/local/bin/
-rm ~/Downloads/xplr-$platform.tar.gz
+sudo mv xplr /usr/local/bin/
+rm xplr-$platform.tar.gz
 curl -o ~/.config/xplr/init.lua https://raw.githubusercontent.com/agileguy/cli-setup/refs/heads/main/xplr/init.lua
